86c9389fb9c1ea07045a58e2f92206e681186464
at-Shreya <miss.shreyabarai@gmail.com>
2026-09-12 16:33:13 +0600

    Add data and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Hero />
+
     </>
   );
 }
